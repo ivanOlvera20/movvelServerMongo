95ddf011a7bc1bd668878296b6c9a3c7056c5df7
Ivan Olvera <ivanolvera@IvanOlvera.local>
2020-02-10 23:04:43 -0600

    "

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb+srv://ivanolvera:phmp9814@cluster0-q14lz.mongodb.net/test?retryWrites=true&w=majority"
-
 const connectDb = async () => {
-  await mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+  await mongoose.connect(
+    process.env.MONGO_URI ||
+      "mongodb+srv://ivanolvera:phmp9814@cluster0-q14lz.mongodb.net/test?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+  );
   console.log("db connected with MongoDB Atlas")
 }
 module.exports = connectDb;
