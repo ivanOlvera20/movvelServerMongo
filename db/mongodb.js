61f53f854475678966dcd2db38596f590ebacec6
Ivan Olvera <ivanolvera@IvanOlvera.local>
2020-02-11 00:08:07 -0600

    b

--- db/mongodb.js
+++ db/mongodb.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   await mongoose.connect(
     process.env.MONGODB_URI ||
-      "mongodb+srv://ivanolvera:phmp9814@cluster0-q14lz.mongodb.net/test?retryWrites=true&w=majority",
+      "mongodb://ivanolvera:phmp9815@ds119660.mlab.com:19660/heroku_xkj9b26r",
     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
   );
-  console.log("db connected with MongoDB Atlas")
-}
+  console.log("db connected with MongoDB Atlas");
+};
 module.exports = connectDb;
