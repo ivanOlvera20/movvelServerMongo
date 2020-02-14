4bef5116d9f853c0ee5c3d6378bb7b5a4efb2c00
Ivan Olvera <ivanolvera@IvanOlvera.local>
2020-02-14 14:18:18 -0600

    auto linea

--- models/cat_produccion/linea.js
+++ models/cat_produccion/linea.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const { MongooseAutoIncrementID } = require("mongoose-auto-increment-reworked");
 
 const lineaSchema = new Schema(
   {
@@ -10,6 +11,16 @@ const lineaSchema = new Schema(
     timestamps: true
   }
 );
+MongooseAutoIncrementID.initialise("counters");
 
+lineaSchema.plugin(MongooseAutoIncrementID.plugin, {
+  modelName: "linea",
+  field: "clave",
+  incrementBy: 1,
+  startAt: 100,
+  unique: true,
+  nextCount: false,
+  resetCount: false
+});
 
 module.exports = model("linea", lineaSchema);
