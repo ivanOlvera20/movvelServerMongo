0a738bee3c20f79d8a580ae92ba4cf7fd887a324
Ivan Olvera <ivanolvera@IvanOlvera.local>
2020-02-14 13:56:55 -0600

    autoincrement familia

--- models/cat_produccion/familia.js
+++ models/cat_produccion/familia.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const { MongooseAutoIncrementID } = require("mongoose-auto-increment-reworked");
 
 const familiaSchema = new Schema(
     {
@@ -6,6 +7,15 @@ const familiaSchema = new Schema(
         descripcion: { type: String, required: true },
     }, {
     timestamps: true
+});
+familiaSchema.plugin(MongooseAutoIncrementID.plugin, {
+    modelName: "familia",
+    field: "clave",
+    incrementBy: 1,
+    startAt: 100,
+    unique: true,
+    nextCount: false,
+    resetCount: false
 });
 
 module.exports = model('familia', familiaSchema)
